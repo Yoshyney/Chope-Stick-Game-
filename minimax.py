7eddd5153cff3421a8f23fd259ad3c326c7db4eb
steven <stevenchiffe@gmail.com>
2019-08-08 17:19:47 +0200

    Adding some random to the plays

--- minimax.py
+++ minimax.py
@@ -1,9 +1,12 @@
 
+import random
+
 def Max(sticks, depth, t = 1):
     total = sticks
     coup = 0
     max = -100
     val = -99
+    coups = []
     if depth == 0:
         return Rules(sticks)
     else:
@@ -12,10 +15,16 @@ def Max(sticks, depth, t = 1):
             if sticks > 0:
                 val = Min(sticks, depth - 1)
             sticks = total
+            if val == max or x == 1 and t == 1:
+                coups.append(x)
             if val > max:
                 max = val
                 coup = x
+                if x != 1 and t == 1:
+                    coups.clear()
     if t == 1:
+        if len(coups) > 1 and t == 1:
+            coup = random.choices(coups)[0]
         return str(coup)
     else:
         return max
